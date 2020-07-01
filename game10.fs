8c9df9623cab758ee82f07cfe36acb6cd88ce815
pm62eg <33402112+pm62eg@users.noreply.github.com>
2020-07-01 17:53:22 +0100

    added comments, sligh restructuring, repeat game

--- game10.fs
+++ game10.fs
@@ -1,29 +1,51 @@
+\ Funky game 'game10'.
+\ The game board consists of 10 empty cells.
+\ For each move the computer chooses a random character,
+\ possibly a repeat from earlier moves,
+\ and the player puts it in one empty cell, keeping
+\ the already filled-in cells in alphabetical order.
+\ When there is no possible move, the game is over.
+\ The score is the number of filled-in cells.
+
+\ My (possibly faulty) implementation of xoshiro256ss
 s" xoshiro256ss.fs" required
+
+\ xoshiro256ss returns a 64-bit pseudo random number
+: saverandom64 ( -- )
+    xoshiro256ss here 10 + ! ;
+
+\ Assume here+10 starts off with a large enough number < 26^10
+\ otherwise the last letters will all be 'a'
+: randletter ( -- c )
+    here 10 + @                        \ get random value
+    26 /mod                            \ mod and quotient
+    here 10 + !                        \ save rest of random value
+    [char] a + ;                       \ adjust return character
 
 : printcell ( n -- )
     here + c@ emit 32 emit ;
 
-: printgame ( -- )
+: printboard ( -- )
     10 0 do i printcell loop ;
 
 : occupied? ( n -- f )
     here + c@ [char] - <> ;
 
-: leftgreater? ( c n -- f )
+: leftgreater? ( c n -- f )            \ TODO invert condition, simplify
     dup 0 = if
        2drop false exit then
     dup 1 - occupied? if
        1 - here + c@ < else
        1 - recurse then ;
 
-: rightsmaller? ( c n -- f )
+: rightsmaller? ( c n -- f )           \ TODO invert condition, simplify
     dup 9 = if
        2drop false exit then
     dup 1 + occupied? if
        1 + here + c@ > else
        1 + recurse then ;
 
-: validmove? ( c n -- f )
+: validmove? ( c n -- f )              \ TODO simplify, refactor
     dup occupied? if
        2drop false exit then
     2dup leftgreater? if
@@ -32,37 +54,49 @@ s" xoshiro256ss.fs" required
        2drop false exit then
     2drop true ;
 
-: randletter ( -- c )
-    here 10 + @                     \ get random value
-    26 /mod                         \ mod and quotient
-    here 10 + !                     \ save rest of random value
-    [char] a + ;                    \ adjust return character
-
-: prompt ( c -- )                   \ prompt for c
+\ Prompt for next move with c character
+: prompt ( c -- )
     ." pos for '" emit ." '? " ;
 
-: getmove ( -- n )                  \ get player's move
+\ Get player's move converted to integer 0 to 9
+: getmove ( -- n )
     begin
        key dup
        [char] 0 < over [char] 9 > or while
        drop
-    repeat dup emit cr [char] 0 - ;
+    repeat dup emit cr [char] 0 - ;    \ echo user choice
 
+\ Calculate game score for the current board
 : gamescore ( -- n )
     0 10 0 do
        i occupied? if 1 + then
     loop ;
 
-: game10 ( -- )
-    xoshiro256-timeseed
-    xoshiro256ss here 10 + !         \ save 64 bits
-    here 10 [char] - fill
-    cr ." Can you score 10 points?" cr ." 0 1 2 3 4 5 6 7 8 9" cr
+\ Print intro, initialize playing board with 10 '-'
+: boardinit ( -- )
+    cr
+    ." Place random letters in free cells," cr
+    ." keeping them in alphabetical order." cr
+    ." Can you score 10 points?" cr
+    cr
+    ." 0 1 2 3 4 5 6 7 8 9" cr
+    here 10 [char] - fill ;
+
+: playgame ( -- )
+    saverandom64
+    boardinit
     begin
-       printgame
+       printboard
        randletter dup prompt getmove
        2dup validmove? dup if
            -rot here + c! else
            -rot 2drop then
     invert gamescore 10 = or until
-    ." You scored " gamescore . ." points." ;
+    ." You scored " gamescore . ." points." cr ;
+
+: game10 ( -- )
+    xoshiro256-timeseed                \ seed random number generator
+    begin
+       playgame
+       cr ." Another (Y/n)? "
+    key [char] n = until ;

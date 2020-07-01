48b4823f9c1d51a12a6a4f1f2639c5b2d71a7d91
pm62eg <33402112+pm62eg@users.noreply.github.com>
2020-07-01 15:27:04 +0100

    consistent indentation, begin ... until

--- game10.fs
+++ game10.fs
@@ -1,7 +1,7 @@
 s" xoshiro256ss.fs" required
 
 : printcell ( n -- )
-   here + c@ emit 32 emit ;
+    here + c@ emit 32 emit ;
 
 : printgame ( -- )
     10 0 do i printcell loop ;
@@ -38,25 +38,24 @@ s" xoshiro256ss.fs" required
     [char] a + ;                    \ adjust return character
 
 : getpos ( X -- X n )               \ print 'X' and ask for 0 to 9
-   ." pos for '" dup emit ." '? "
-   key [char] 0 - cr ;              \ TODO validation
+    ." pos for '" dup emit ." '? "
+    key [char] 0 - cr ;             \ TODO validation
 
 : gamescore ( -- n )
-   0 10 0 do
-      i occupied? if 1 + then
-   loop ;
+    0 10 0 do
+       i occupied? if 1 + then
+    loop ;
 
 : game10 ( -- )
-   xoshiro256-timeseed
-   xoshiro256ss here 10 + !         \ save 64 bits
-   here 10 [char] - fill
-   cr ." Can you score 10 points?" cr ." 0 1 2 3 4 5 6 7 8 9" cr
-   begin
-      printgame
-      randletter getpos
-      2dup validmove? dup if
-          -rot here + c! else
-          -rot 2drop then
-      gamescore 10 < and while
-   repeat
-   ." You scored " gamescore . ." points." ;
+    xoshiro256-timeseed
+    xoshiro256ss here 10 + !         \ save 64 bits
+    here 10 [char] - fill
+    cr ." Can you score 10 points?" cr ." 0 1 2 3 4 5 6 7 8 9" cr
+    begin
+       printgame
+       randletter getpos
+       2dup validmove? dup if
+           -rot here + c! else
+           -rot 2drop then
+    invert gamescore 10 = or until
+    ." You scored " gamescore . ." points." ;

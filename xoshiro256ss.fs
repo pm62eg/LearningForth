d80a724cd76dad6f6e121f323e31b034f5d793ee
pm62eg <33402112+pm62eg@users.noreply.github.com>
2020-07-01 19:41:00 +0100

    tweaked initialization

--- xoshiro256ss.fs
+++ xoshiro256ss.fs
@@ -6,10 +6,10 @@ create xstate 4 cells allot
 
 : xoshiro256-timeseed ( -- )
     time&date + + + + +
-    dup 17 lshift xstate 0 cells + !
-    dup 19 lshift xstate 1 cells + !
-    dup 23 lshift xstate 2 cells + !
-        29 lshift xstate 3 cells + ! ;
+    dup 23 lshift over xor xstate 0 cells + !
+    dup 31 lshift over xor xstate 1 cells + !
+    dup 39 lshift over xor xstate 2 cells + !
+    dup 43 lshift      xor xstate 3 cells + ! ;
 
 : xoshiro256ss ( -- u )
     xstate 1 cells + @ dup 5 * 7 rol64 9 *
